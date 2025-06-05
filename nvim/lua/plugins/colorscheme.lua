1d30738f68e0f05faa16621bd4e4f7dd9aea817b
hrsvrn <harshvardhanvatsa@gmail.com>
2025-06-05 15:30:12 +0530

    random commit

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,18 @@
-return{
+return {
   "folke/tokyonight.nvim",
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    -- load the colorscheme here
     require("tokyonight").setup({
-})
-
+      style = "night", -- or "storm", "moon", "day"
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      -- You can add other options here if needed
+    })
     vim.cmd.colorscheme("tokyonight")
   end,
 }
+
